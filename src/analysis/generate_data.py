8b910372be32e8d74ca65f3635202e95d5fcad68
pmx-chen <pequalsmx.chen@gmail.com>
2025-04-25 14:41:50 +0800

    add files

---
+++ src/analysis/generate_data.py
@@ -0,0 +1,16 @@
+# %%
+import pandas as pd
+import os
+from pathlib import Path
+
+
+# %%
+data_dir = {
+    'stock_price': 'C:\Users\pequa\Dropbox (Personal)\Research\my-project-shared\disposition-learning\src\data\Daily Stock Price Returns'
+}
+
+base_price = pd.read_csv('/src/data/Daily Stock Price Returns/TRD_Dalyr.csv')
+
+
+
+# %%
